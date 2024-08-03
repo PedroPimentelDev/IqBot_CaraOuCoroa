170a7ccd724c77e23c3afc2be8748ee520890d22
Pedro Pimentel <162853878+PedroPimentelDev@users.noreply.github.com>
2024-08-03 15:32:06 -0300

    Improved methods

--- Funçoes.py
+++ Funçoes.py
@@ -1,9 +1,16 @@
 from iqoptionapi.stable_api import IQ_Option
-Iq = IQ_Option("","") # Email e senha
-conectar = Iq.connect()
 import random
 
-#Defini o cara ou coroa
+def conectarIq(email, senha):
+    iq = IQ_Option(email, senha)
+    conectado = iq.connect()
+    if conectado[0]:
+        print('Conectado')
+    else:
+        print('Não foi possível conectar')
+    return iq, conectado[0]
+
+#Escolhe a direção de entrada
 def estrategia():
     caracoroa = random.randint(1, 2)
     if caracoroa == 1:
@@ -15,37 +22,33 @@ def estrategia():
     return dir
 
 #Realiza a compra
-def comprar(moeda, dir, valor, exp, tipo):
+def comprar(iq, moeda, dir, valor, exp, tipo):
     if tipo == "DIGITAL":
-        id = Iq.buy_digital_spot_v2(moeda, valor, dir, exp)
+        id = iq.buy_digital_spot_v2(moeda, valor, dir, exp)
     else:
-        id = Iq.buy(valor, moeda, dir, exp)
+        id = iq.buy(valor, moeda, dir, exp)
     return id[1]
 
 #Verifica o resultado da operacao em andamento
-def verificarResultado(tipo, id):
+def verificar_resultado(iq, tipo, id):
     while True:
         if tipo == "DIGITAL":
-            resultado = Iq.check_win_digital_v2(id)
+            resultado = iq.check_win_digital_v2(id)
         elif tipo == "BINARY":
-            resultado = Iq.check_win_v4(id)
+            resultado = iq.check_win_v4(id)
         else:
-            break
+            return "Tipo de operação inválido", 0
 
-        if resultado[1] is not None and resultado[1] > 0:
-            msg = ("Vitória =)", resultado[1])
-            break
-        elif resultado[1] is not None and resultado[1] < 0:
-            msg = ("Derrota =( Vamos pra próxima", resultado[1])
-            break
-        elif resultado[1] is not None and resultado[1] == 0:
-            msg = ("Derrota =( Vamos pra próxima", resultado[1])
-            break
-    return msg
+        if resultado[1] is not None:
+            lucro = round(resultado[1], 2)
+            if lucro > 0:
+                msg = "Vitória =)"
+            else:
+                msg = "Derrota =( Vamos pra próxima"
+            return msg, lucro
 
 #Resume em uma unica funcao 
-def executarOperacao(moeda, valor, exp, tipo):
+def executarOperacao(iq, moeda, valor, exp, tipo):
     dir = estrategia()
-    id = comprar(moeda, dir, valor, exp, tipo)
-    msg = verificarResultado(tipo, id)
-    return(msg)
+    id = comprar(iq, moeda, dir, valor, exp, tipo)
+    return verificar_resultado(iq, tipo, id)
